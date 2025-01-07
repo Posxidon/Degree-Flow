2134f81f7e7698c81dcf2d69e5e094de43db3084
Harshit Sharma <harshitsharma967@gmail.com>
2025-01-07 00:24:12 -0500

    Updated backend framework

---
+++ degreeflow-backend/degreeflow-backend/src/test/java/com/degreeflow/DegreeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degreeflow;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+public class DegreeFlowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        // Ensure the Spring Boot application context loads successfully
+    }
+}
