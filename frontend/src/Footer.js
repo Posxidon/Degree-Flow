c4ec3df6a0b09ee8b962fc591dfc50b6032a6d26
anupamraj0900 <70150362+anupamraj0900@users.noreply.github.com>
2025-03-24 00:59:45 -0400

    Add SeatAlertPage and update Footer styling

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import './Footer.css';
-import mesLogo from './Logo_Name_White.png'; // or wherever your MES logo is
 
 function Footer() {
   return (
     <footer className="footer">
-      {/* Left Column */}
-      <div className="footer-col footer-left">
-        <img src={mesLogo} alt="MES Logo" className="footer-logo" />
-      </div>
+      <div className="footer-wrapper">
+        <div className="footer-links">
+          <a href="/contact">Contact Us</a>
+          <a href="/privacy">Privacy Policy</a>
+        </div>
 
-      {/* Center Column */}
-      <div className="footer-col footer-center">
-        <a href="/contact">CONTACT US</a>
-        <br />
-        <a href="/privacy">PRIVACY POLICY</a>
-        {/* If you want T&C as well: */}
-        {/* <a href="/terms">TERMS & CONDITIONS</a> */}
-      </div>
-
-      {/* Right Column */}
-      <div className="footer-col footer-right">
-        1280 Main Street West Hamilton, Ontario L8S 4L8
+        <p className="footer-copy">
+          ©
+          {' '}
+          {new Date().getFullYear()}
+          {' '}
+          DegreeFlow. All rights reserved.
+        </p>
       </div>
     </footer>
   );
