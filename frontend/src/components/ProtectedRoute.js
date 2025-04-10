2e01d817cc3097a86169dc43755eeb90a2d71f2e
Harshit Sharma <sharmh17@mcmaster.ca>
2025-04-10 00:39:02 -0400

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -12,7 +12,7 @@ function ProtectedRoute({ component: Component, requiredRoles }) {
     const checkUserRole = async () => {
       if (isAuthenticated && user) {
         try {
-          const roles = user['http://localhost:3000/roles'] || []; // Get roles from the JWT claim
+          const roles = user['https://degreeflow.netlify.app/roles'] || []; // Get roles from the JWT claim
           const hasRequiredRole = requiredRoles.some((role) => roles.includes(role));
           setHasRole(hasRequiredRole);
         } catch (error) {
