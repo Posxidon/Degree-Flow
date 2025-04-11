a97f244d2c47b53c0d3962fc773f53e9a0d3ec3c
Anupam Raj <70150362+anupamraj0900@users.noreply.github.com>
2025-04-10 23:14:19 -0400

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -12,7 +12,7 @@ function ProtectedRoute({ component: Component, requiredRoles }) {
     const checkUserRole = async () => {
       if (isAuthenticated && user) {
         try {
-          const roles = user['https://degree-flow.vercel.app//roles'] || []; // Get roles from the JWT claim
+          const roles = user['https://degree-flow.vercel.app/roles'] || []; // Get roles from the JWT claim
           const hasRequiredRole = requiredRoles.some((role) => roles.includes(role));
           setHasRole(hasRequiredRole);
         } catch (error) {
